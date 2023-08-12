c6c45f14898388da7b57d5d9d247292f93a56da0
Sumonta Saha Mridul <61287791+Sumonta056@users.noreply.github.com>
2023-08-12 20:35:53 +0600

    Add : Min-Max Algorithm

--- Lab Final Code/6-Astar.cpp
+++ Lab Final Code/6-Astar.cpp
@@ -1,10 +1,11 @@
 #include <bits/stdc++.h>
+
 using namespace std;
 
 class Graph
 {
     map<int, vector<pair<int, int>>> l;
-    map<int, int> nodeHeuristics; // Heuristic value for each node
+    map<int, int> nodeHeuristics;
 
 public:
     void addEdge(int node1, int node2, int cost)
@@ -20,35 +21,32 @@ public:
 
     void A_star_search(int source, int target)
     {
-        priority_queue<pair<int, int>, vector<pair<int, int>>, greater<pair<int, int>>> pq;
+        priority_queue<pair<pair<int, int>, int>, vector<pair<pair<int, int>, int>>, greater<pair<pair<int, int>, int>>> pq;
         map<int, int> gCost; // Cost from start node to current node
-        map<int, int> fCost; // gCost + heuristic cost
         map<int, int> parent; // To store the parent node for each visited node
 
         for (auto nodePair : l)
         {
             int node = nodePair.first;
             gCost[node] = INT_MAX;
-            fCost[node] = INT_MAX;
             parent[node] = -1;
         }
 
-        pq.push({0, source});
+        pq.push({{0, heuristic(source, target, 0)}, source}); // Pair of gCost + heuristic
         gCost[source] = 0;
-        fCost[source] = heuristic(source, target, 0); // Initial cost is 0
 
         while (!pq.empty())
         {
             int current = pq.top().second;
             pq.pop();
 
-            cout << "Visiting node " << current << " with heuristic value " << fCost[current] - gCost[current] << endl;
+            cout << "Visiting node " << current <<  " with f-cost " << pq.top().first.second << endl;
 
             if (current == target)
             {
                 cout << "Target reached!" << endl;
                 cout << "Total cost to reach target: " << gCost[target] << endl;
-                printPath(parent, target); // Print the path to reach the target
+                printPath(parent, target);
                 return;
             }
 
@@ -61,8 +59,7 @@ public:
                 if (new_gCost < gCost[neighbour])
                 {
                     gCost[neighbour] = new_gCost;
-                    fCost[neighbour] = heuristic(neighbour, target, gCost[neighbour]) + new_gCost; // Updated the calculation here
-                    pq.push({fCost[neighbour], neighbour});
+                    pq.push({{new_gCost, heuristic(neighbour, target, new_gCost)}, neighbour});
                     parent[neighbour] = current;
                 }
             }
@@ -73,7 +70,6 @@ public:
 
     int heuristic(int node, int target, int currentCost)
     {
-        // Use the heuristic value specified for the node
         return nodeHeuristics[node] + currentCost;
     }
 
@@ -100,28 +96,50 @@ int main()
 {
     Graph g;
 
-    g.addEdge(0, 1, 75);
-    g.addEdge(0, 2, 118);
-    g.addEdge(0, 4, 140);
-    g.addEdge(4, 5, 99);
-    g.addEdge(4, 6, 80);
-    g.addEdge(6, 7, 97);
-    g.addEdge(7, 8, 101);
-    g.addEdge(5, 8, 120);
+    // g.addEdge(0, 1, 75);
+    // g.addEdge(0, 2, 118);
+    // g.addEdge(0, 4, 140);
+    // g.addEdge(2, 3, 111);
+    // g.addEdge(4, 5, 99);
+    // g.addEdge(4, 6, 80);
+    // g.addEdge(6, 7, 97);
+    // g.addEdge(7, 8, 101);
+    // g.addEdge(5, 8, 211);
 
-    int source = 0, target = 8;
+    // int source = 0, target = 8;
 
-    g.setHeuristic(0, 366); // Set heuristic value for node 0
-    g.setHeuristic(1, 374);
-    g.setHeuristic(2, 329);
-    g.setHeuristic(3, 244);
-    g.setHeuristic(4, 253);
-    g.setHeuristic(5, 178);
-    g.setHeuristic(6, 193);
-    g.setHeuristic(7, 98);
-    g.setHeuristic(8, 0);
+    g.addEdge(0, 1, 4);
+    g.addEdge(0, 2, 3);
+    g.addEdge(1, 5, 5);
+    g.addEdge(1, 4, 12);
+    g.addEdge(2, 4, 10);
+    g.addEdge(2, 3, 7);
+    g.addEdge(3, 4, 2);
+    g.addEdge(4, 6, 5);
+    g.addEdge(5, 6, 16);
+
+     int source = 0, target = 6;
+
+    // g.setHeuristic(0, 366);
+    // g.setHeuristic(1, 374);
+    // g.setHeuristic(2, 329);
+    // g.setHeuristic(3, 244);
+    // g.setHeuristic(4, 253);
+    // g.setHeuristic(5, 178);
+    // g.setHeuristic(6, 193);
+    // g.setHeuristic(7, 98);
+    // g.setHeuristic(8, 0);
+
+     g.setHeuristic(0, 14);
+    g.setHeuristic(1, 12);
+    g.setHeuristic(2, 11);
+    g.setHeuristic(3, 6);
+    g.setHeuristic(4, 4);
+    g.setHeuristic(5, 11);
+    g.setHeuristic(6, 0);
+  
 
     g.A_star_search(source, target);
 
-    return 0;
+
 }
